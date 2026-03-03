845367de29c896c3a0b419fdd740a6e052774998
Pasindu Subasinghe <phimansa@gmail.com>
2026-03-03 13:17:17 +0530

    Tab navigator set up

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+const Index = () => {
+  return (
+    <View>
+      <Text>Todos</Text>
+    </View>
+  );
+};
+export default Index;
